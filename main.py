1a9f1b82db1ece5905b273fcdbbce7934594ec53
edwardtheiron <hellishfire>
2022-08-07 00:17:28 +0300

    Added filing xml with proper data

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import os
 
 # folder path
-dir_path = r'C:\\c\\'
-
+dir_path = r"C:\\d\\"
+template_path = r"C:\\d\\template.xml"
+placeholder = "placeholder"
+file_count = 0
 # list to store files
-res = []
+# res = []
 
-# Iterate directory
+# Iterate target directory
 for path in os.listdir(dir_path):
     # check if current path is a file
     if os.path.isfile(os.path.join(dir_path, path)):
-        pathtemp = path #using temp var in case I'll need full list later
-        if ".igs" in pathtemp: #check only for igs files
-            pathtemp.removesuffix(".igs")
-            pathtemp = pathtemp + ".xml"
-            #print(pathtemp)
-            if not os.path.exists(pathtemp):
-                open(dir_path + pathtemp, 'w')
-                #os.mknod(pathtemp)
-                #print("file \'" + pathtemp + "\' created.")
+        #checks only for igs files
+        if ".IGS" in path:                
+            base_name = path.removesuffix(".IGS") # 
+            xml_name = base_name + ".xml"
+            #check if xml already exists
+            if not os.path.exists(xml_name):
+                with open(template_path,'r') as template_file, open(dir_path + xml_name, 'w') as new_xml:                        
+                    for line in template_file:                           
+                        if placeholder in line:
+                            line = line.replace(placeholder,base_name)
+                        #else:
+                            #print(f"No {placeholder} in file")
+                        new_xml.write(line)
+                file_count = file_count + 1
+                new_xml.close()
+                template_file.close()
+                #print("file \'" + path + "\' created.")
             #else:
-                #print("file \'" + pathtemp + "\' exists.")
-        #res.append(path) -- adds up to list
-#print(res) -- total list
+                #print("file \'" + path + "\' exists.")
+print(str(file_count) + " files created")
