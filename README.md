e8f6a460226d9fb7586540f45df1a819f8e8d3df
edwardtheiron <hellishfire>
2022-08-12 21:51:49 +0300

    __main__, recursive dir processor, fix of minor things

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # xml-to-pair-igs
-Tool that creates XML file for IGS file. (Railworks/Train Simulator Blueprints)
+Tool that creates XML file for IGS file from given template file. (Railworks/Train Simulator Blueprints)
--- main.py
+++ main.py
@@ -1,34 +1,67 @@
+from genericpath import isdir
 import os
 
-# folder path
-dir_path = r"C:\\d\\"
+#added __name__ == "__main__"
+#added recursive dir processor
+#fixed minor things
+#bugs: looses subdirs for GeomID
+#todo: relative paths, terminal args
+
+#temp global vars
 template_path = r"C:\\d\\template.xml"
 placeholder = "placeholder"
 file_count = 0
-# list to store files
-# res = []
 
-# Iterate target directory
-for path in os.listdir(dir_path):
-    # check if current path is a file
-    if os.path.isfile(os.path.join(dir_path, path)):
-        #checks only for igs files
-        if ".IGS" in path:                
-            base_name = path.removesuffix(".IGS") # 
-            xml_name = base_name + ".xml"
-            #check if xml already exists
-            if not os.path.exists(xml_name):
-                with open(template_path,'r') as template_file, open(dir_path + xml_name, 'w') as new_xml:                        
-                    for line in template_file:                           
-                        if placeholder in line:
-                            line = line.replace(placeholder,base_name)
-                        #else:
-                            #print(f"No {placeholder} in file")
-                        new_xml.write(line)
-                file_count = file_count + 1
-                new_xml.close()
-                template_file.close()
-                #print("file \'" + path + "\' created.")
-            #else:
-                #print("file \'" + path + "\' exists.")
-print(str(file_count) + " files created")
+def dir_processor(dir_path):
+    print(os.listdir(dir_path))
+    # Iterate target directory    
+    for path in os.listdir(dir_path):
+        #print("path is " + path)
+        # check if current path is a folder
+        
+        joined_path = os.path.join(dir_path, path)
+        #print("joined_path is " + joined_path)
+        if os.path.isdir(joined_path):
+            #print("dir_processor: --" + dir_path + "--")
+            dir_processor(joined_path)
+        if os.path.isfile(joined_path):
+            #print("file_processor: --" + dir_path + "-- and path --" + path + "--")
+            file_processor(dir_path, path)
+
+def file_processor(dir_path, path):
+    print("FP's dir_path " + dir_path)
+    print("FP's path     " + path)
+    #checks only for igs files
+    if ".IGS" in path:
+        base_name = path.removesuffix(".IGS")
+        xml_name = base_name + ".xml"
+        #check if xml already exists
+        if not os.path.exists(xml_name):
+            with open(template_path,'r') as template_file, open(os.path.join(dir_path, xml_name), 'w') as new_xml:                        
+                for line in template_file:
+                    #print(type(line))                           
+                    if placeholder in line:
+                        line = line.replace(placeholder,base_name)
+                    #else:
+                        #print(f"No {placeholder} in file")
+                    new_xml.write(line)
+            global file_count
+            file_count = file_count + 1
+            new_xml.close()
+            template_file.close()
+            #print("file \'" + xml_name + "\' created.")
+        #else:
+            #print("file \'" + xml_name + "\' exists.")        
+
+def xml_creator():    
+    # folder path
+    dir_path = r"C:\\d\\" 
+    
+    # list to store files
+    # res = []
+    dir_processor(dir_path)
+
+    print(str(file_count) + " files created")
+
+if __name__ == "__main__":
+    xml_creator()
