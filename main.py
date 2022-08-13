7527f45b58a7f18e4ba3722192e25cd75c109d71
edwardtheiron <hellishfire>
2022-08-13 14:01:39 +0300

    Proper subdir path for igs inside of xml

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
 from genericpath import isdir
 import os
 
-#added __name__ == "__main__"
-#added recursive dir processor
-#fixed minor things
-#bugs: looses subdirs for GeomID
+#fixed "looses subdirs for GeomID"
+#bugs: 1.file counter works wrong 2.ignores old xmls
 #todo: relative paths, terminal args
 
 #temp global vars
 template_path = r"C:\\d\\template.xml"
 placeholder = "placeholder"
+path_placeholder = "PATH_PLACEHOLDER"
 file_count = 0
 
 def dir_processor(dir_path):
-    print(os.listdir(dir_path))
     # Iterate target directory    
     for path in os.listdir(dir_path):
-        #print("path is " + path)
-        # check if current path is a folder
-        
+        # check if current path is a folder        
         joined_path = os.path.join(dir_path, path)
-        #print("joined_path is " + joined_path)
         if os.path.isdir(joined_path):
-            #print("dir_processor: --" + dir_path + "--")
             dir_processor(joined_path)
         if os.path.isfile(joined_path):
-            #print("file_processor: --" + dir_path + "-- and path --" + path + "--")
             file_processor(dir_path, path)
 
 def file_processor(dir_path, path):
-    print("FP's dir_path " + dir_path)
-    print("FP's path     " + path)
     #checks only for igs files
-    if ".IGS" in path:
-        base_name = path.removesuffix(".IGS")
+    if ".igs" in path.lower():
+        base_name = path.lower().removesuffix(".igs")
         xml_name = base_name + ".xml"
         #check if xml already exists
         if not os.path.exists(xml_name):
@@ -44,14 +35,41 @@ def file_processor(dir_path, path):
                         line = line.replace(placeholder,base_name)
                     #else:
                         #print(f"No {placeholder} in file")
+                    if path_placeholder in line:
+                        adapted_path = adapt_path(os.path.join(dir_path, path))
+                        line = line.replace(path_placeholder,adapted_path)
+                    #else:
+                        #print(f"No {placeholder} in file")
+                    #print(os.path.join(dir_path, path))                
                     new_xml.write(line)
             global file_count
             file_count = file_count + 1
             new_xml.close()
             template_file.close()
-            #print("file \'" + xml_name + "\' created.")
-        #else:
-            #print("file \'" + xml_name + "\' exists.")        
+            print("file \'" + xml_name + "\' created.")
+        else:
+            print("file \'" + xml_name + "\' exists.")
+
+def adapt_path(dir_path):
+    dir_path = dir_path.lower().split(r"\\")
+    x = 0 # iterator
+    skip = 0 # skip flag
+    path = 0 # flag and path, init with 0 so there won't be opening with backslash
+    while x < len(dir_path):    
+        if dir_path[x] == "source": #called once we find TS specific dir
+            if skip == 1:
+                #bad path, there should be only one source dir
+                print("path is incorrect. source dir is doubled")
+            x = x + 3
+            skip = 1
+        if skip == 1:
+            #print(dir_path[x])
+            if path == 0:
+                path = dir_path[x]
+            else:
+                path = path + "\\" + dir_path[x]
+        x = x + 1
+    return path    
 
 def xml_creator():    
     # folder path
